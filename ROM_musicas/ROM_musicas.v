e24f3b770f39d30adfefc1c8232c8229eceec563
Davi Felix <davifelix@usp.br>
2022-11-27 21:51:18 -0300

    create ROM memory to store musics

---
+++ ROM_musicas/ROM_musicas.v
@@ -0,0 +1,16 @@
+module ROM_musicas(
+  input wire[23:0] addr, // endereço a ser retornado
+  output wire[7:0] data // palavra da memória
+);
+
+  parameter max_addr = {24{1'b1}}; // 2^24 - 1 
+
+  reg[7:0] mem[0: max_addr]; // Array de 2^24 paralavras de 8 bits
+
+  initial begin
+    $readmemb("musics.dat", mem); // Inicializa a memória com um arquivo
+  end
+
+  assign data = mem[addr]; // Retorna o elemento da memória no endereço addr
+
+endmodule
